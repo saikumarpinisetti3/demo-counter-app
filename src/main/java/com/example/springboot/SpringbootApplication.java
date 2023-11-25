60ec749deb796ad03d08fdcc73fd6348ebe87286
saikumarpinisetti3 <132870970+saikumarpinisetti3@users.noreply.github.com>
2023-11-25 11:15:12 +0530

    Update SpringbootApplication.java

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,14 +1,41 @@
-package com.example.springboot;
+import static spark.Spark.*;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class CalculatorApp {
+    public static void main(String[] args) {
+        staticFiles.location("/public");
 
-@SpringBootApplication
-public class SpringbootApplication {
+        get("/", (req, res) -> {
+            res.redirect("/index.html");
+            return null;
+        });
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+        post("/calculate", (req, res) -> {
+            double num1 = Double.parseDouble(req.queryParams("num1"));
+            double num2 = Double.parseDouble(req.queryParams("num2"));
+            String operator = req.queryParams("operator");
 
-	}
+            double result = calculate(num1, num2, operator);
 
+            return "Result: " + result;
+        });
+    }
+
+    private static double calculate(double num1, double num2, String operator) {
+        switch (operator) {
+            case "add":
+                return num1 + num2;
+            case "subtract":
+                return num1 - num2;
+            case "multiply":
+                return num1 * num2;
+            case "divide":
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    return Double.NaN; // Indicate division by zero
+                }
+            default:
+                return Double.NaN; // Invalid operator
+        }
+    }
 }
